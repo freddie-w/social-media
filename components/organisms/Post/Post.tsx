60b064a66e0ae08a6e2a698761b9a8e3ebd77aed
Freddie Wellfair <fred_w2k9@hotmail.com>
2021-10-16 15:55:24 +0100

    build on commit & passing input props

--- components/organisms/Post/Post.tsx
+++ components/organisms/Post/Post.tsx
@@ -27,14 +27,6 @@ const dropdownItems = [
   },
 ];
 
-const author = {
-  name: "Freddie Wellfair",
-  href: "#",
-  imageUrl:
-    "https://images.unsplash.com/photo-1519345182560-3f2917c472ef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-  alt: "Name",
-};
-
 const initialComments = [
   {
     id: 1234,
